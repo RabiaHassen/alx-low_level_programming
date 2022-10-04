4ff806f1ab8a6e3ad188949469c7e22ed4f3d431
Rabia Hassen <rabia-hassen@hotmail.com>
2022-10-04 06:20:36 +0000

    task 3 and task 4

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,6 +7,7 @@ void simple_print_buffer(char *buffer, unsigned int size);
 char *_strdup(char *str);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+void print_grid(int **grid, int width, int height)
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 #endif
