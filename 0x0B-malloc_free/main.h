e18f7321f12fe65ec622ba7aa7a8302cc5e5bf54
Rabia Hassen <rabia-hassen@hotmail.com>
2022-10-04 06:41:25 +0000

    task 3 and 4 fix

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,12 +2,10 @@
 #define MAIN_H
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-char *str_concat(char *s1, char *s2);
-void simple_print_buffer(char *buffer, unsigned int size);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-void print_grid(int **grid, int width, int height)
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 #endif
